17439086172511f025607a12074fb7f6e8d0a87b
laerciocandido <lcs_junior@yahoo.com.br>
2020-10-28 02:25:10 -0300

    calculos dos juros

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import css from './form.module.css';
+import Installments from '../Installments/Installments';
 
 export default function Form() {
+	const [capital, setCapital] = useState(0);
+	const [juros, setJuros] = useState(0);
+	const [tempo, setTempo] = useState(0);
+	const [parcela, setParcela] = useState([]);
+
+	const handleChangeCapital = (event) => {
+		setCapital(event.target.value);
+		//console.log(capital);
+	}
+
+	const handleChangeJuros = (event) => {
+		setJuros(event.target.value)
+		//console.log(juros);
+	}
+
+	const handleChangeTempo = (event) => {
+		setTempo(event.target.value);
+		//console.log(tempo);
+	}
+
+	//let parcelax =[]
+	useEffect(() => {
+		let capitalInicial = capital;
+		let montante = capital;
+		let jurosx = 0;
+		let porcento = 0;
+		let aux =[];
+
+		for (let i = 0; i < tempo; i++)
+		{
+			montante = (montante * (1 + juros/100)).toFixed(2);
+			jurosx = (montante - capitalInicial).toFixed(2);
+			porcento = ((montante/capitalInicial - 1)*100).toFixed(2);
+			aux.push({montante, jurosx, porcento});
+			//setParcela({montante, jurosx, porcento});
+		}
+		setParcela(aux);
+
+	}, [capital, juros, tempo])
+
 	return (
 		<div className={css.container}>
 			<h3>Calculadora de Juros Compostos</h3>
@@ -11,7 +52,8 @@ export default function Form() {
 					<input
 						autoFocus
 						type="number"
-						//value={fullSalary}						//onChange={this.handleFilter}
+						value={capital}
+						onChange={handleChangeCapital}
 						step="10"
 						/>
 				</div>
@@ -20,7 +62,8 @@ export default function Form() {
 					<input
 						autoFocus
 						type="number"
-						//value={fullSalary}						//onChange={this.handleFilter}
+						value={juros}
+						onChange={handleChangeJuros}
 						step="0.1"
 						/>
 				</div>
@@ -29,11 +72,15 @@ export default function Form() {
 					<input
 					autoFocus
 						type="number"
-						//value={fullSalary}						//onChange={this.handleFilter}
+						value={tempo}
+						onChange={handleChangeTempo}
 						step="1"
 						/>
 				</div>
 			</div>
+			< Installments
+				parcela={parcela}
+			/>
 		</div>
 	)
 }
---
+++ src/components/Installments/Installments.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Installments({parcela}) {
+	//const {parcela} = props;
+	/*
+	let html = "<ul>";
+	for (let i = 0; i < tempo; i++)
+		html += `<li> ${juros} </li>`
+	html += "</ul>"
+	*/
+	console.log(parcela)
+	return (
+		<div>
+
+		</div>
+	)
+}
